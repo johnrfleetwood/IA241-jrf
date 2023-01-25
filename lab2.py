f4294bde932f588ba4c4c682920d6b84bf95c12b
EC2 Default User <ec2-user@ip-172-31-29-145.ec2.internal>
2023-01-25 17:22:52 +0000

    John Fleetwood lab2.py File

---
+++ lab2.py
@@ -0,0 +1,40 @@
+''' 
+IA 241 Lab 2 
+John Fleetwood 
+1/25/2023
+'''
+
+#6.1
+my_name = 'Tom'
+print(my_name.upper())
+
+#6.2
+my_id = 123
+print(my_id)
+
+#6.3
+# 123 = my_id
+
+#6.4
+my_id = your_id = 123
+print(my_id)
+
+#6.5
+my_id_str = '123'
+print(my_id_str)
+
+#6.6
+#print(my_name + my_id)
+
+#6.7
+print(my_name + my_id_str)
+
+#6.8
+print(my_name * 3)
+
+#6.9
+print('hello, world. This is my first python string'.split('.'))
+
+#6.10
+message = 'Tom's id is 123'
+print(message)
---
+++ lec2.py
@@ -0,0 +1,34 @@
+'''
+Lecture 2 of IA 241
+Topic: Numbers Strings and Variables
+Date: 1/24/2023
+'''
+
+'''Variable Types'''
+print("hello") # single line comment (string variable)
+print(123) # integer variable
+print( type(123))
+print( type('hello')) # type tells that variable it is
+print( type(123.123))
+
+'''String Methods'''
+print('hello ') #single vs doulbe quotation does not matter
+print("it's the second python class") # can use apostrophy in the string
+print('Hello World'.upper()) #adding .upper capitizes all letters
+print('     hello      world      '.strip()) #gets rid of spaces in front at back of string
+print('hello word')
+print('hello word'.split()) #default, split by spaces
+print('hello world'.split('o')) #split by 'o'
+print('hello' + " world") # + adds the str
+
+'''Math Section'''
+print(3*3) 
+print(3-3)
+print(3/3)
+print(3+3)
+
+'''Data Container Section'''
+my_num = 123 #stores a variable
+print(my_num) #calls stored var
+my_num = 234
+print(my_num)
