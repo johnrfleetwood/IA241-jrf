28084c0a559165e3beec8c5887a244896ef55281
EC2 Default User <ec2-user@ip-172-31-29-145.ec2.internal>
2023-03-23 20:03:22 +0000

    John Fleetwood lab9_class.py & lab9_instances.py files

--- lab9_instance.py
+++ lab9_instance.py
@@ -11,3 +11,8 @@ print(my_cal.cal_sigma(5,3))
 print(my_cal.cal_pi(5,3))
 print(my_cal.cal_fac(5))
 print(my_cal.cal_p(5,2))
+
+#3.3
+import numpy as np
+print (np.math.factorial(999))
+print(my_cal.cal_fac(999))
